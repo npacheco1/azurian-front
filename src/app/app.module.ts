fd15668414c38b9d2572d7e113afb2462787bd1d
Nelson Pacheco <nelson.pacheco@tecinco.cl>
2019-07-20 18:30:15 -0400

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http'
+import {UserService} from './user.service'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
