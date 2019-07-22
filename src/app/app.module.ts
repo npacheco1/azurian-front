b1fe93b2cf9bc3d39637d144389bd8ac097ec542
Nelson Pacheco <nelson.pacheco@tecinco.cl>
2019-07-22 13:38:21 -0400

    datatable con api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 import {UserService} from './user.service'
 import { AppComponent } from './app.component';
-
+import { DataTableComponent } from './data-table/data-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { DataTablesModule } from 'angular-datatables';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    DataTablesModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
